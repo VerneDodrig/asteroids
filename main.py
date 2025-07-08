674a792f89bdfc89d2570f018ec021f5e04cc0b4
VerneDodrig <damion-arends@hotmail.com>
2025-07-07 22:01:22 -0700

    created a screen that's just black and allowed closing the window to close the game.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from constants import *
 
 def main():
     pygame.init()
-    pygame.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(0)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
